9b75febaf6f4bd9b6ade1c2717ba46a29d5dc64d
systnager <systnager@gmail.com>
2023-09-25 21:31:31 +0300

    delete redundant print

--- database.py
+++ database.py
@@ -17,9 +17,6 @@ cursor = conn.cursor()
 def write_new_item(table: str, columns: list, values: list):
     columns_clause = ', '.join([f"{col}" if isinstance(col, str) else str(col) for col in columns])
     values_clause = ', '.join([f"'{val}'" if isinstance(val, str) else str(val) for val in values])
-    print(f"INSERT INTO user ('id', 'state', 'balance') VALUES (6517009257, 'default', 0);")
-
-    print(f'INSERT INTO {table} ({columns_clause}) VALUES ({values_clause});')
     cursor.execute(f'INSERT INTO {table} ({columns_clause}) VALUES ({values_clause});')
     conn.commit()
 
@@ -34,7 +31,6 @@ def get_item(table, columns, params=None, param_values=None):
                                                                                    param_values is None) else ''
     set_clause = "*" if columns == '*' else ", ".join(columns)
     where_clause = " WHERE " + where_clause if where_clause else ""
-    print(f"SELECT {set_clause} FROM {table} {where_clause};")
     cursor.execute(f"SELECT {set_clause} FROM {table} {where_clause};")
     return cursor.fetchall()
 
@@ -51,6 +47,5 @@ def update_item(table, columns, values, params=None, param_values=None):
                                 )
 
     where_clause = " WHERE " + where_clause if where_clause else ""
-    print(f"UPDATE {table} SET {set_clause} {where_clause};")
     cursor.execute(f"UPDATE {table} SET {set_clause} {where_clause};")
     conn.commit()
