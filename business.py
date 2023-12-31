e59fd3cede30e42261c3130d3375ec1ba67445f7
systnager <systnager@gmail.com>
2023-12-31 16:25:53 +0200

    add functions for working with advcash exchanging

--- business.py
+++ business.py
@@ -43,7 +43,19 @@ def set_payeer_usd_to_uah_course(course):
     save_config(config)
 
 
+def set_advcash_usd_to_uah_course(course):
+    config = get_config()
+    config["advcash_usd_to_uah"] = course
+    save_config(config)
+
+
 def set_payeer_account(payeer_account):
     config = get_config()
     config["payeer_account"] = payeer_account
+    save_config(config)
+
+
+def set_advcash_account(advcash_account):
+    config = get_config()
+    config["advcash_account"] = advcash_account
     save_config(config)
